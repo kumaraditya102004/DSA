db4b38f542e9be8c3fc59c26f35c35b538c65d2c
Aditya Kumar <kumar.aditya102004@gmail.com>
2026-09-08 11:26:38 +0530

    Initial commit

---
+++ SinglyLinkedList.java
@@ -0,0 +1,57 @@
+
+public class SinglyLinkedList {
+
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Insert at end
+    void insert(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    // Display
+    void display() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.insert(100);
+        list.insert(200);
+        list.insert(300);
+        list.insert(400);
+
+        list.display();
+    }
+}
