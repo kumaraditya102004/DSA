d20f82a0590a5db2a4b8528735e7e3540532d57b
Aditya Kumar <kumar.aditya102004@gmail.com>
2026-09-08 11:30:04 +0530

    Initial commit

---
+++ DoublyLinkedList.java
@@ -0,0 +1,101 @@
+
+class DoublyLinkedList {
+
+    // Node class
+    class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    Node head = null;
+
+    // Insert at beginning
+    void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    // Insert at end
+    void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+        newNode.prev = temp;
+    }
+
+    // Forward traversal
+    void displayForward() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+
+        System.out.println();
+    }
+
+    // Backward traversal
+    void displayBackward() {
+        if (head == null) {
+            return;
+        }
+
+        Node temp = head;
+
+        // Go to last node
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        // Traverse backward
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.prev;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        DoublyLinkedList list = new DoublyLinkedList();
+
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+
+        list.insertAtBeginning(5);
+
+        System.out.println("Forward:");
+        list.displayForward();
+
+        System.out.println("Backward:");
+        list.displayBackward();
+    }
+}
